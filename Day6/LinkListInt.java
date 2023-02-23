9313a1af0cca5a0f6b7a135556df29a9dc0f6974
Arpit Gupta <arpitgalahadgupta@gmail.com>
2023-02-23 17:07:44 +0530

    Add files via upload

---
+++ Day6/LinkListInt.java
@@ -0,0 +1,12 @@
+package JAVA.Day6;
+import java.util.*;
+
+public class LinkListInt {
+        public static void main(String args[]){
+                LinkedList<String> linklist = new LinkedList<String>();
+                linklist.add("Hello");
+                linklist.add("World");
+                linklist.add("India");
+                linklist.add("Philines");
+        }
+}
